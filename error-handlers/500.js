be41fa04c1231f298234eebb9f4be3fb07393c19
Yazeed Aloufee <yazeedaloufee@gmail.com>
2021-09-18 19:57:50 +0300

    INITIATE node app

---
+++ error-handlers/500.js
@@ -0,0 +1,11 @@
+'use strict'
+
+module.exports=(err,req,res, next)=>{
+
+    res.status(500).json({
+        status:500,
+        message:err.message,
+        route:req.path
+
+    })
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict'
+const server = require('./server.js')
+require('dotenv').config();
+const port = process.env.PORT||3000;
+
+server.start(port)
